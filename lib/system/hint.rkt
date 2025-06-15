5ff8fe6282438a7d1c79d713c75defd0d4874a79
Cutie Deng <CutieDeng@iCloud.com>
2025-06-15 19:26:10 +0800

    (FEAT "YIELD")

--- lib/system/hint.rkt
+++ lib/system/hint.rkt
@@ -4,8 +4,10 @@
 (require "../util/into-int.rkt")
 (require "../util/try-from-int.rkt")
 
+(define NOP-head #x354)
+
 (define NOP-literal (bitwise-ior
-  (arithmetic-shift #x354 22)
+  (arithmetic-shift NOP-head 22)
   (arithmetic-shift #x3 16)
   (arithmetic-shift #x2 12)
   #x1f
@@ -27,3 +29,30 @@
 )
 
 (provide (struct-out NOP))
+
+(define YIELD-head NOP-head)
+
+(define YIELD-literal (bitwise-ior
+  (arithmetic-shift YIELD-head 22)
+  (arithmetic-shift #x3 16)
+  (arithmetic-shift #x2 12)
+  (arithmetic-shift #x1 5)
+  #x1f
+))
+
+(define (int->YIELD i)
+  (cond [(nand (equal? i YIELD-literal)) #f]
+    [else (YIELD)])
+)
+
+(define (YIELD->int _nop)
+  YIELD-literal)
+
+(struct YIELD ()
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int YIELD->int
+  #:property prop:try-from-int int->YIELD
+)
+
+(provide (struct-out YIELD))
