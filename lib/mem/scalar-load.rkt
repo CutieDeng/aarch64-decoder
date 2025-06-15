48be351eea1e8faf1f9470e4e58a918b75140270
Cutie Deng <CutieDeng@iCloud.com>
2025-06-15 21:28:15 +0800

    (FEAT "LDR")

---
+++ lib/mem/scalar-load.rkt
@@ -0,0 +1,54 @@
+#lang racket
+
+(require "../util/in-feature.rkt")
+(require "../util/into-int.rkt")
+(require "../util/try-from-int.rkt")
+
+(define (int->LDR/r/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 26 27)
+    (bitwise-bit-field i 22 24)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 13 16)
+    (bitwise-bit-field i 12 13)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDR/r i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 10 12) 2)
+  ) #f]
+  [else (apply LDR/r (int->LDR/r/struct i))])
+)
+
+(define (LDR/r->int l)
+  (match-define (LDR/r size vr opc rm option s rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift vr 26)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift option 13)
+    (arithmetic-shift s 12)
+    (arithmetic-shift #x2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDR/r (size vr opc rm option s rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDR/r->int
+  #:property prop:try-from-int int->LDR/r
+)
+
+(provide (struct-out LDR/r))
