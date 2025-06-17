682e6d8f4de57cd44f634e73834ccdb8e7d5a375
Cutie Deng <CutieDeng@iCloud.com>
2025-06-17 22:49:05 +0800

    (feat "MRS" "Add MRS, MSR(register)")
    ("Support general-purpose register to System Regsiter or reverse")

---
+++ lib/branch/system.rkt
@@ -0,0 +1,86 @@
+#lang racket
+
+(require "../util/in-feature.rkt")
+(require "../util/into-int.rkt")
+(require "../util/try-from-int.rkt")
+
+(define (int->MRS/struct i)
+  (list (bitwise-bit-field i 19 20) 
+    (bitwise-bit-field i 16 19)
+    (bitwise-bit-field i 12 16)
+    (bitwise-bit-field i 8 12)
+    (bitwise-bit-field i 5 8)
+    (bitwise-bit-field i 0 5)
+    )
+)
+
+(define MRS-head #x354)
+
+(define (int->MRS i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 22 32) MRS-head)
+    (equal? (bitwise-bit-field i 21 22) #x1)
+    (equal? (bitwise-bit-field i 20 21) #x1)
+    ) #f]
+    [else (apply MRS (int->MRS/struct i))])
+)
+
+(define (MRS->int e)
+  (match-define (MRS o0 op1 crn crm op2 rt) e)
+  (bitwise-ior
+    (arithmetic-shift MRS-head 22)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift #x1 20)
+    (arithmetic-shift o0 19)
+    (arithmetic-shift op1 16)
+    (arithmetic-shift crn 12)
+    (arithmetic-shift crm 8)
+    (arithmetic-shift op2 5)
+    rt
+  )
+)
+
+(struct MRS (o0 op1 crn crm op2 rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int MRS->int
+  #:property prop:try-from-int int->MRS
+)
+
+(provide (struct-out MRS))
+
+(define int->MSR/r/struct int->MRS/struct)
+
+(define MSR/r-head MRS-head)
+
+(define (int->MSR/r i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 22 32) MSR/r-head)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 20 21) #x1)
+    ) #f]
+    [else (apply MSR/r (int->MSR/r/struct i))])
+)
+
+(define (MSR/r->int e)
+  (match-define (MSR/r o0 op1 crn crm op2 rt) e)
+  (bitwise-ior
+    (arithmetic-shift MSR/r-head 22)
+    (arithmetic-shift #x1 20)
+    (arithmetic-shift o0 19)
+    (arithmetic-shift op1 16)
+    (arithmetic-shift crn 12)
+    (arithmetic-shift crm 8)
+    (arithmetic-shift op2 5)
+    rt
+  )
+)
+
+(struct MSR/r (o0 op1 crn crm op2 rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int MSR/r->int
+  #:property prop:try-from-int int->MSR/r
+)
+
+(provide (struct-out MSR/r))
