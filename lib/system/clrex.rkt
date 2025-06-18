ebe10bf90c7806358aecb7bba70c9e8b4c745862
Cutie Deng <CutieDeng@iCloud.com>
2025-06-18 15:04:44 +0800

    (feat "ISB" "instruction synchronization barrier")

--- lib/system/clrex.rkt
+++ lib/system/clrex.rkt
@@ -101,3 +101,36 @@
 )
 
 (provide (struct-out DSB))
+
+(define ISB-head CLREX-head)
+
+(define int->ISB/struct int->CLREX/struct)
+
+(define (int->ISB i)
+  (cond [(nand (equal? (bitwise-bit-field i 12 32) ISB-head)
+    (equal? (bitwise-bit-field i 7 8) #x1)
+    (equal? (bitwise-bit-field i 5 7) #x2)
+    (equal? (bitwise-bit-field i 0 5) #x1f))
+    #f]
+    [else (apply ISB (int->ISB/struct i))])
+)
+
+(define (ISB->int cl)
+  (match-define (ISB crm) cl)
+  (bitwise-ior
+    (arithmetic-shift ISB-head 12)
+    (arithmetic-shift crm 8)
+    (arithmetic-shift #x1 7)
+    (arithmetic-shift #x2 5)
+    #x1f
+  )
+)
+
+(struct ISB (crm)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int ISB->int
+  #:property prop:try-from-int int->ISB
+)
+
+(provide (struct-out ISB))
