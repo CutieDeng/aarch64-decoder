57e98c9ed43da3fcc6277370e1bb4c41488bd95f
Cutie Deng <CutieDeng@iCloud.com>
2025-06-19 09:26:15 +0800

    (feat "LDR/i/Post" "post index load register")

--- lib/mem/scalar-load.rkt
+++ lib/mem/scalar-load.rkt
@@ -61,3 +61,47 @@
     [#x111 'SXTX]
   )
 )
+
+(define (int->LDR/i/Post/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 26 27)
+    (bitwise-bit-field i 22 24)
+    (bitwise-bit-field i 12 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDR/i/Post i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) 1)
+  ) #f]
+  [else (apply LDR/i/Post (int->LDR/i/Post/struct i))])
+)
+
+(define (LDR/i/Post->int l)
+  (match-define (LDR/i/Post size vr opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift vr 26)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDR/i/Post (size vr opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDR/i/Post->int
+  #:property prop:try-from-int int->LDR/i/Post
+)
+
+(provide (struct-out LDR/i/Post))
