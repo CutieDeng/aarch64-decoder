2f4d9e98ca346f0aaa6f8974d27d0370106cc67a
Cutie Deng <CutieDeng@iCloud.com>
2025-06-21 09:34:04 +0800

    (FEAT "LDR/i/Unsigned")

--- lib/mem/scalar-load.rkt
+++ lib/mem/scalar-load.rkt
@@ -142,3 +142,46 @@
 )
 
 (provide (struct-out LDR/i/Pre))
+
+(define (int->LDR/i/Unsigned/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 26 27)
+    (bitwise-bit-field i 22 24)
+    (bitwise-bit-field i 10 22)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5)
+  )
+)
+
+(define (int->LDR/i/Unsigned i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 1)
+    (equal? (bitwise-bit-field i 22 24) 1)
+  ) #f]
+  [else (apply LDR/i/Unsigned (int->LDR/i/Unsigned/struct i))])
+)
+
+(define (LDR/i/Unsigned->int l)
+  (match-define (LDR/i/Unsigned size vr opc imm12 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift vr 26)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm12 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDR/i/Unsigned (size vr opc imm12 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDR/i/Unsigned->int
+  #:property prop:try-from-int int->LDR/i/Unsigned
+)
+
+(provide (struct-out LDR/i/Unsigned))
