a39520f42fb3443d8bb5e6756c08bf702b5bf80d
Cutie Deng <CutieDeng@iCloud.com>
2025-06-23 22:23:19 +0800

    (feat "LDRSH" "LDRSH(register)")

--- lib/mem/scalar-load.rkt
+++ lib/mem/scalar-load.rkt
@@ -676,3 +676,43 @@
 )
 
 (provide (struct-out LDRH/i/Unsigned))
+
+(define int->LDRSH/r/struct int->LDRH/r/struct)
+
+(define (int->LDRSH/r i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 23 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 10 12) 2)
+  ) #f]
+  [else (apply LDRSH/r (int->LDRSH/r/struct i))])
+)
+
+(define (LDRSH/r->int l)
+  (match-define (LDRSH/r size opc rm option s rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift option 13)
+    (arithmetic-shift s 12)
+    (arithmetic-shift #x2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDRSH/r (size opc rm option s rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDRSH/r->int
+  #:property prop:try-from-int int->LDRSH/r
+)
+
+(provide (struct-out LDRSH/r))
+
