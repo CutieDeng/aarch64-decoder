ed0e2a8e00772b5f2d828e884bc79ab962146b21
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 17:23:41 +0800

    (feat "LDLARB" "STRLR")

--- lib/mem/lor.rkt
+++ lib/mem/lor.rkt
@@ -206,3 +206,42 @@
 )
 
 (provide (struct-out STRLRH))
+
+(define int->STRLR/struct int->LDLARB/struct)
+
+(define (int->STRLR i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) #x1)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply STRLR (int->STRLR/struct i))])
+)
+
+(define (STRLR->int l)
+  (match-define (STRLR size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift #x1 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STRLR (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LOR . #t))
+  #:property prop:into-int STRLR->int
+  #:property prop:try-from-int int->STRLR
+)
+
+(provide (struct-out STRLR))
