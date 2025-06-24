3535f14dde821521e8ea0e93e02668df4a209b2b
Cutie Deng <CutieDeng@iCloud.com>
2025-06-25 02:20:59 +0800

    (FEAT "LDLAR")

--- lib/mem/lor.rkt
+++ lib/mem/lor.rkt
@@ -89,3 +89,42 @@
 )
 
 (provide (struct-out LDLARH))
+
+(define int->LDLAR/struct int->LDLARB/struct)
+
+(define (int->LDLAR i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) #x1)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply LDLAR (int->LDLAR/struct i))])
+)
+
+(define (LDLAR->int l)
+  (match-define (LDLAR size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift #x1 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDLAR (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LOR . #t))
+  #:property prop:into-int LDLAR->int
+  #:property prop:try-from-int int->LDLAR
+)
+
+(provide (struct-out LDLAR))
