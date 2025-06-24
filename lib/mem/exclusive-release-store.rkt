51e8b1d3c3cfb07abb0df67de496b9d0354af97c
Cutie Deng <CutieDeng@iCloud.com>
2025-06-25 02:06:15 +0800

    (FEAT "STLXP")

--- lib/mem/exclusive-release-store.rkt
+++ lib/mem/exclusive-release-store.rkt
@@ -122,3 +122,49 @@
 )
 
 (provide (struct-out STLXRH))
+
+(define (int->STLXP/struct i)
+  (list (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 15 16)
+    (bitwise-bit-field i 10 15)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->STLXP i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+  ) #f]
+  [else (apply STLXP (int->STLXP/struct i))])
+)
+
+(define (STLXP->int l)
+  (match-define (STLXP sz l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift 1 31)
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STLXP (sz l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int STLXP->int
+  #:property prop:try-from-int int->STLXP
+)
+
+(provide (struct-out STLXP))
