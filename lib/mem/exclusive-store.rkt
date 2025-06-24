63473db4c6d43bebc732268fd6f4ed9d80588394
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 15:45:03 +0800

    (feat "STXR" "STXRH")

--- lib/mem/exclusive-store.rkt
+++ lib/mem/exclusive-store.rkt
@@ -85,3 +85,40 @@
 )
 
 (provide (struct-out STXRB))
+
+(define int->STXRH/struct int->STXR/struct)
+
+(define (int->STXRH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply STXRH (int->STXRH/struct i))])
+)
+
+(define (STXRH->int l)
+  (match-define (STXRH size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STXRH (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int STXRH->int
+  #:property prop:try-from-int int->STXRH
+)
+
+(provide (struct-out STXRH))
