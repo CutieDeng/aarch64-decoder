a856de3deffd8d078769df4a889d49d739562308
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 16:27:52 +0800

    (feat "LDAR" "LDARH")

--- lib/mem/acquire-load.rkt
+++ lib/mem/acquire-load.rkt
@@ -465,3 +465,42 @@
 )
 
 (provide (struct-out LDARB))
+
+(define int->LDARH/struct int->LDAR/struct)
+
+(define (int->LDARH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) #x2)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 1)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply LDARH (int->LDARH/struct i))])
+)
+
+(define (LDARH->int l)
+  (match-define (LDARH size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift #x1 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDARH (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDARH->int
+  #:property prop:try-from-int int->LDARH
+)
+
+(provide (struct-out LDARH))
