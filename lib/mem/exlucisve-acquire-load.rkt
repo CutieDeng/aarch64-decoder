671974f148b90fcd745763a651f1c326be777501
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 16:55:24 +0800

    (feat "LDAXR" "LDAXRH")

--- lib/mem/exlucisve-acquire-load.rkt
+++ lib/mem/exlucisve-acquire-load.rkt
@@ -87,3 +87,41 @@
 )
 
 (provide (struct-out LDAXRB))
+
+(define int->LDAXRH/struct int->LDAXR/struct)
+
+(define (int->LDAXRH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply LDAXRH (int->LDAXRH/struct i))])
+)
+
+(define (LDAXRH->int l)
+  (match-define (LDAXRH size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDAXRH (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDAXRH->int
+  #:property prop:try-from-int int->LDAXRH
+)
+
+(provide (struct-out LDAXRH))
