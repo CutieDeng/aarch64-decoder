a4816bde380b315b2eb7ecb223c79157096bb98b
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 21:25:05 +0800

    (FEAT "STRH/r")

--- lib/mem/scalar-store.rkt
+++ lib/mem/scalar-store.rkt
@@ -314,3 +314,42 @@
 )
 
 (provide (struct-out STRB/i/Unsigned))
+
+(define int->STRH/r/struct int->STR/r/struct)
+
+(define (int->STRH/r i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 10 12) #x2)
+  ) #f]
+  [else (apply STRH/r (int->STRH/r/struct i))])
+)
+
+(define (STRH/r->int l)
+  (match-define (STRH/r size opc rm option s rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift option 13)
+    (arithmetic-shift s 12)
+    (arithmetic-shift #x2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STRH/r (size opc rm option s rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int STRH/r->int
+  #:property prop:try-from-int int->STRH/r
+)
+
+(provide (struct-out STRH/r))
