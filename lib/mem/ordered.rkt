f8273481a4abf159c22256f6c5d6c9c239b8878a
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 17:14:27 +0800

    (feat "STILP" "STILP")

--- lib/mem/ordered.rkt
+++ lib/mem/ordered.rkt
@@ -50,3 +50,49 @@
 )
 
 (provide (struct-out LDIAPP))
+
+(define (int->STILP/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 12 16)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->STILP i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 23 26) #x2)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 13 16) 0)
+    (equal? (bitwise-bit-field i 10 12) #x2)
+  ) #f]
+  [else (apply STILP (int->STILP/struct i))])
+)
+
+(define (STILP->int l)
+  (match-define (STILP size l rt2 opc2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x2 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rt2 16)
+    (arithmetic-shift opc2 12)
+    (arithmetic-shift #x2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STILP (size l rt2 opc2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LRCPC3 . #t))
+  #:property prop:into-int STILP->int
+  #:property prop:try-from-int int->STILP
+)
+
+(provide (struct-out STILP))
