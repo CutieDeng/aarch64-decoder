f49db7fabbcc250ae5679c6e72930ff66f1cedc3
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 10:57:50 +0800

    (feat "STURB" "STURB")

--- lib/mem/unscaled-store.rkt
+++ lib/mem/unscaled-store.rkt
@@ -79,3 +79,38 @@
 )
 
 (provide (struct-out STURB))
+
+(define int->STURH/struct int->STUR/struct)
+
+(define (int->STURH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply STURH (int->STURH/struct i))])
+)
+
+(define (STURH->int l)
+  (match-define (STURH size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STURH (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int STURH->int
+  #:property prop:try-from-int int->STURH
+)
+
+(provide (struct-out STURH))
