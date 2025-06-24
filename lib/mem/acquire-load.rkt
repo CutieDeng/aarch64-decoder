83feb77e564be14dab18edac7f6cf28b86f51b7e
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 15:56:58 +0800

    (feat "LDAPR" "LDAPRB")

--- lib/mem/acquire-load.rkt
+++ lib/mem/acquire-load.rkt
@@ -50,3 +50,45 @@
 )
 
 (provide (struct-out LDAPR))
+
+(define int->LDAPRB/struct int->LDAPR/struct)
+
+(define (int->LDAPRB i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 0)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 23 24) 1)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) #x4)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDAPRB (int->LDAPRB/struct i))])
+)
+
+(define (LDAPRB->int l)
+  (match-define (LDAPRB size rs rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift #x1 23)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift #x1 15)
+    (arithmetic-shift #x4 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDAPRB (size rs rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LRCPC . #t))
+  #:property prop:into-int LDAPRB->int
+  #:property prop:try-from-int int->LDAPRB
+)
+
+(provide (struct-out LDAPRB))
