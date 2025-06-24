1b29c9f84dfa691c361dd8f55e23bea5397f7261
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 16:58:18 +0800

    (feat "LDAXR" "LDAXP")

--- lib/mem/exlucisve-acquire-load.rkt
+++ lib/mem/exlucisve-acquire-load.rkt
@@ -125,3 +125,50 @@
 )
 
 (provide (struct-out LDAXRH))
+
+(define (int->LDAXP/struct i)
+  (list (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 15 16)
+    (bitwise-bit-field i 10 15)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDAXP i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 1)
+  ) #f]
+  [else (apply LDAXP (int->LDAXP/struct i))])
+)
+
+(define (LDAXP->int l)
+  (match-define (LDAXP sz l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift 1 31)
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift #x1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDAXP (sz l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDAXP->int
+  #:property prop:try-from-int int->LDAXP
+)
+
+(provide (struct-out LDAXP))
