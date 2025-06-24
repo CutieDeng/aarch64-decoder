e4875b6236c4d864ab7ade53aff9a7f81fdc3e1c
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 13:23:36 +0800

    (feat "STTR" "STTRH")

--- lib/mem/unprivileged-store.rkt
+++ lib/mem/unprivileged-store.rkt
@@ -81,3 +81,39 @@
 )
 
 (provide (struct-out STTRB))
+
+(define int->STTRH/struct int->STTR/struct)
+
+(define (int->STTRH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) #x2)
+  ) #f]
+  [else (apply STTRH (int->STTRH/struct i))])
+)
+
+(define (STTRH->int l)
+  (match-define (STTRH size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift #x2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STTRH (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int STTRH->int
+  #:property prop:try-from-int int->STTRH
+)
+
+(provide (struct-out STTRH))
