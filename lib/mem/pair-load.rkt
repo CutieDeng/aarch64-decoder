98d631bafca49bc95ebc8244b26a1eb52ec130c8
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 11:02:05 +0800

    (feat "LDP/Post" "fix LDP")

--- lib/mem/pair-load.rkt
+++ lib/mem/pair-load.rkt
@@ -4,7 +4,7 @@
 (require "../util/into-int.rkt")
 (require "../util/try-from-int.rkt")
 
-(define (int->LDP/struct i)
+(define (int->LDP/Post/struct i)
   (list (bitwise-bit-field i 30 32)
     (bitwise-bit-field i 26 27)
     (bitwise-bit-field i 22 24)
@@ -15,18 +15,18 @@
     (bitwise-bit-field i 0 5))
 )
 
-(define (int->LDP i)
+(define (int->LDP/Post i)
   (cond [(nand (equal? (bitwise-bit-field i 30 31) 0)
     (equal? (bitwise-bit-field i 27 30) #x5)
     (equal? (bitwise-bit-field i 26 27) 0)
     (equal? (bitwise-bit-field i 23 26) 1)
     (equal? (bitwise-bit-field i 22 23) 1)
   ) #f]
-  [else (apply LDP (int->LDP/struct i))])
+  [else (apply LDP/Post (int->LDP/Post/struct i))])
 )
 
-(define (LDP->int l)
-  (match-define (LDP opc l imm7 rt2 rn rt) l)
+(define (LDP/Post->int l)
+  (match-define (LDP/Post opc l imm7 rt2 rn rt) l)
   (bitwise-ior
     (arithmetic-shift opc 30)
     (arithmetic-shift #x5 27)
@@ -39,11 +39,11 @@
   )
 )
 
-(struct LDP (opc l imm7 rt2 rn rt)
+(struct LDP/Post (opc l imm7 rt2 rn rt)
   #:transparent
   #:property prop:in-feature #hash()
-  #:property prop:into-int LDP->int
-  #:property prop:try-from-int int->LDP
+  #:property prop:into-int LDP/Post->int
+  #:property prop:try-from-int int->LDP/Post
 )
 
-(provide (struct-out LDP))
+(provide (struct-out LDP/Post))
