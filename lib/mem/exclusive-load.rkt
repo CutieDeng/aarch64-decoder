2c47fad9a0d14c393216a29f16823ea9d6442213
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 23:30:08 +0800

    (FEAT "LDXRH")

--- lib/mem/exclusive-load.rkt
+++ lib/mem/exclusive-load.rkt
@@ -87,3 +87,41 @@
 )
 
 (provide (struct-out LDXRB))
+
+(define int->LDXRH/struct int->LDXR/struct)
+
+(define (int->LDXRH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply LDXRH (int->LDXRH/struct i))])
+)
+
+(define (LDXRH->int l)
+  (match-define (LDXRH size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDXRH (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDXRH->int
+  #:property prop:try-from-int int->LDXRH
+)
+
+(provide (struct-out LDXRH))
