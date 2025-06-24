cffd4e7c95b90524f1912b98c41d6573ecf44423
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 16:00:56 +0800

    (feat "LDAPR" "LDAPUR")

--- lib/mem/acquire-load.rkt
+++ lib/mem/acquire-load.rkt
@@ -134,3 +134,42 @@
 )
 
 (provide (struct-out LDAPRH))
+
+(define (int->LDAPUR/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 22 24)
+    (bitwise-bit-field i 12 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDAPUR i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x19)
+    (equal? (bitwise-bit-field i 22 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDAPUR (int->LDAPUR/struct i))])
+)
+
+(define (LDAPUR->int l)
+  (match-define (LDAPUR size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x19 24)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDAPUR (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LRCPC2 . #t))
+  #:property prop:into-int LDAPUR->int
+  #:property prop:try-from-int int->LDAPUR
+)
+
+(provide (struct-out LDAPUR))
