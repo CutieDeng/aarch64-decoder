efaf42ea390dee3ceb614151a779b55b6ba2c16a
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 10:39:11 +0800

    (feat "LDURB" "LDURB")

--- lib/mem/unscaled-load.rkt
+++ lib/mem/unscaled-load.rkt
@@ -44,3 +44,38 @@
 )
 
 (provide (struct-out LDUR))
+
+(define int->LDURB/struct int->LDUR/struct)
+
+(define (int->LDURB i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 0)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDURB (int->LDURB/struct i))])
+)
+
+(define (LDURB->int l)
+  (match-define (LDURB size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDURB (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDURB->int
+  #:property prop:try-from-int int->LDURB
+)
+
+(provide (struct-out LDURB))
