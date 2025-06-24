9ad16f2e77b23d4ad2c6762c444a01dc2404dd1d
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 22:05:30 +0800

    (FEAT "LDP/Pre")
    "fix: LDP/Post"

--- lib/mem/pair-load.rkt
+++ lib/mem/pair-load.rkt
@@ -6,11 +6,9 @@
 
 (define (int->LDP/Post/struct i)
   (list (bitwise-bit-field i 30 32)
-    (bitwise-bit-field i 26 27)
-    (bitwise-bit-field i 22 24)
-    (bitwise-bit-field i 16 21)
-    (bitwise-bit-field i 13 16)
-    (bitwise-bit-field i 12 13)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 15 22)
+    (bitwise-bit-field i 10 15)
     (bitwise-bit-field i 5 10)
     (bitwise-bit-field i 0 5))
 )
@@ -47,3 +45,38 @@
 )
 
 (provide (struct-out LDP/Post))
+
+(define int->LDP/Pre/struct int->LDP/Post/struct)
+
+(define (int->LDP/Pre i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 31) 0)
+    (equal? (bitwise-bit-field i 27 30) #x5)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 23 26) #x3)
+    (equal? (bitwise-bit-field i 22 23) 1)
+  ) #f]
+  [else (apply LDP/Pre (int->LDP/Pre/struct i))])
+)
+
+(define (LDP/Pre->int l)
+  (match-define (LDP/Pre opc l imm7 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift opc 30)
+    (arithmetic-shift #x5 27)
+    (arithmetic-shift #x3 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift imm7 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDP/Pre (opc l imm7 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDP/Pre->int
+  #:property prop:try-from-int int->LDP/Pre
+)
+
+(provide (struct-out LDP/Pre))
