33af3345112e4d4fbb549fd95e26efd2c6fdbdef
Cutie Deng <CutieDeng@iCloud.com>
2025-06-25 02:23:07 +0800

    (FEAT "STRLRH")

--- lib/mem/lor.rkt
+++ lib/mem/lor.rkt
@@ -167,3 +167,42 @@
 )
 
 (provide (struct-out STLLRB))
+
+(define int->STRLRH/struct int->LDLARB/struct)
+
+(define (int->STRLRH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) #x1)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply STRLRH (int->STRLRH/struct i))])
+)
+
+(define (STRLRH->int l)
+  (match-define (STRLRH size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift #x1 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct STRLRH (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LOR . #t))
+  #:property prop:into-int STRLRH->int
+  #:property prop:try-from-int int->STRLRH
+)
+
+(provide (struct-out STRLRH))
