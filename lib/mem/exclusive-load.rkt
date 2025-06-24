6ca68092d75192ee68974d8fc160e0586f3cb5aa
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 13:29:30 +0800

    (feat "LDXR" "LDXRB")

--- lib/mem/exclusive-load.rkt
+++ lib/mem/exclusive-load.rkt
@@ -7,7 +7,7 @@
 (define (int->LDXR/struct i)
   (list (bitwise-bit-field i 30 32)
     (bitwise-bit-field i 22 23)
-    (bitwise-bit-field i 16 20)
+    (bitwise-bit-field i 16 21)
     (bitwise-bit-field i 15 16)
     (bitwise-bit-field i 10 15)
     (bitwise-bit-field i 5 10)
@@ -49,3 +49,41 @@
 )
 
 (provide (struct-out LDXR))
+
+(define int->LDXRB/struct int->LDXR/struct)
+
+(define (int->LDXRB i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 0)
+    (equal? (bitwise-bit-field i 24 30) #x8)
+    (equal? (bitwise-bit-field i 23 24) 0)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 16 21) #x1f)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 10 15) #x1f)
+  ) #f]
+  [else (apply LDXRB (int->LDXRB/struct i))])
+)
+
+(define (LDXRB->int l)
+  (match-define (LDXRB size l rs o0 rt2 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x8 24)
+    (arithmetic-shift l 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift o0 15)
+    (arithmetic-shift rt2 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDXRB (size l rs o0 rt2 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDXRB->int
+  #:property prop:try-from-int int->LDXRB
+)
+
+(provide (struct-out LDXRB))
