bccdd1c7b038e651aea5e07fcef9f3893f911f29
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 18:36:22 +0800

    (feat "LDR/NEON" "LDR/NEON/Post")

--- lib/mem/neon-load.rkt
+++ lib/mem/neon-load.rkt
@@ -50,3 +50,46 @@
 )
 
 (provide (struct-out LDR/NEON))
+
+(define (int->LDR/NEON/Post/struct i)
+  (list (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 22 24)
+    (bitwise-bit-field i 12 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDR/NEON/Post i)
+  (cond [(nand
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 1)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply LDR/NEON/Post (int->LDR/NEON/Post/struct i))])
+)
+
+(define (LDR/NEON/Post->int l)
+  (match-define (LDR/NEON/Post size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift #x1 26)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDR/NEON/Post (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDR/NEON/Post->int
+  #:property prop:try-from-int int->LDR/NEON/Post
+)
+
+(provide (struct-out LDR/NEON/Post))
