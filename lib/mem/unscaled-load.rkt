c9f391d8bb73ded29424e685bc20e2e6c92448a0
Cutie Deng <CutieDeng@iCloud.com>
2025-06-24 21:40:58 +0800

    (FEAT "LDURH")

--- lib/mem/unscaled-load.rkt
+++ lib/mem/unscaled-load.rkt
@@ -114,3 +114,38 @@
 )
 
 (provide (struct-out LDURSB))
+
+(define int->LDURH/struct int->LDUR/struct)
+
+(define (int->LDURH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 22 24) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDURH (int->LDURH/struct i))])
+)
+
+(define (LDURH->int l)
+  (match-define (LDURH size opc imm9 rn rt) l)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift opc 22)
+    (arithmetic-shift imm9 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDURH (size opc imm9 rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LDURH->int
+  #:property prop:try-from-int int->LDURH
+)
+
+(provide (struct-out LDURH))
