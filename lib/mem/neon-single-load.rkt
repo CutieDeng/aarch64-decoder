adfec4077813a3605919c1be4c2d1a0945f43f51
Cutie Deng <CutieDeng@iCloud.com>
2025-06-25 22:51:43 +0800

    (FEAT "LD1R/Post")

--- lib/mem/neon-single-load.rkt
+++ lib/mem/neon-single-load.rkt
@@ -54,3 +54,54 @@
 )
 
 (provide (struct-out LD1R))
+
+(define (int->LD1R/Post/struct i)
+  (list (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 21 22)
+    (bitwise-bit-field i 16 17)
+    (bitwise-bit-field i 13 16)
+    (bitwise-bit-field i 12 13)
+    (bitwise-bit-field i 10 12)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LD1R/Post i)
+  (cond [(nand
+    (equal? (bitwise-bit-field i 31 32) 0)
+    (equal? (bitwise-bit-field i 23 30) #x1a)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 0)
+    (equal? (bitwise-bit-field i 17 21) 0)
+    (equal? (bitwise-bit-field i 16 17) 0)
+    (equal? (bitwise-bit-field i 13 16) #x6)
+    (equal? (bitwise-bit-field i 12 13) 0)
+  ) #f]
+  [else (apply LD1R/Post (int->LD1R/Post/struct i))])
+)
+
+(define (LD1R/Post->int ld1)
+  (match-define (LD1R/Post q l r rm opcode s size rn rt) ld1)
+  (bitwise-ior
+    (arithmetic-shift q 30)
+    (arithmetic-shift #x1b 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift r 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift opcode 13)
+    (arithmetic-shift s 12)
+    (arithmetic-shift size 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LD1R/Post (q l r rm opcode s size rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LD1R/Post->int
+  #:property prop:try-from-int int->LD1R/Post
+)
+
+(provide (struct-out LD1R/Post))
