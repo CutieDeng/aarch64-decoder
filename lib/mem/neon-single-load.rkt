3e02b0bf68f188c34c5510d46339112b038d0bab
Cutie Deng <CutieDeng@iCloud.com>
2025-06-25 22:56:08 +0800

    (FEAT "LD2R/Post")

--- lib/mem/neon-single-load.rkt
+++ lib/mem/neon-single-load.rkt
@@ -145,3 +145,44 @@
 )
 
 (provide (struct-out LD2R))
+
+(define int->LD2R/Post/struct int->LD1R/Post/struct)
+
+(define (int->LD2R/Post i)
+  (cond [(nand
+    (equal? (bitwise-bit-field i 31 32) 0)
+    (equal? (bitwise-bit-field i 23 30) #x1b)
+    (equal? (bitwise-bit-field i 22 23) 1)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 17 21) 0)
+    (equal? (bitwise-bit-field i 16 17) 0)
+    (equal? (bitwise-bit-field i 13 16) #x6)
+    (equal? (bitwise-bit-field i 12 13) 0)
+  ) #f]
+  [else (apply LD2R/Post (int->LD2R/Post/struct i))])
+)
+
+(define (LD2R/Post->int ld1)
+  (match-define (LD2R/Post q l r rm opcode s size rn rt) ld1)
+  (bitwise-ior
+    (arithmetic-shift q 30)
+    (arithmetic-shift #x1b 23)
+    (arithmetic-shift l 22)
+    (arithmetic-shift r 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift opcode 13)
+    (arithmetic-shift s 12)
+    (arithmetic-shift size 10)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LD2R/Post (q l r rm opcode s size rn rt)
+  #:transparent
+  #:property prop:in-feature #hash()
+  #:property prop:into-int LD2R/Post->int
+  #:property prop:try-from-int int->LD2R/Post
+)
+
+(provide (struct-out LD2R/Post))
