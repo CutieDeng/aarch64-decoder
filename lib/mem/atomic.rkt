76f1d765154774806289c127590faa1762122d55
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 15:51:05 +0800

    (feat "LDADD" "LDADDH")

--- lib/mem/atomic.rkt
+++ lib/mem/atomic.rkt
@@ -89,3 +89,42 @@
 )
 
 (provide (struct-out LDADDB))
+
+(define int->LDADDH/struct int->LDADD/struct)
+
+(define (int->LDADDH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDADDH (int->LDADDH/struct i))])
+)
+
+(define (LDADDH->int ldadd)
+  (match-define (LDADDH size a r rs opc rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDADDH (size a r rs opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int LDADDH->int
+  #:property prop:try-from-int int->LDADDH
+)
+
+(provide (struct-out LDADDH))
