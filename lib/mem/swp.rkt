9820dd9aad5b39c28f760812ba38fdf3a6751b24
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:25:31 +0800

    (feat "SWP" "SWPH")

--- lib/mem/swp.rkt
+++ lib/mem/swp.rkt
@@ -27,8 +27,8 @@
   [else (apply SWP (int->SWP/struct i))])
 )
 
-(define (SWP->int ldadd)
-  (match-define (SWP size a r rs rn rt) ldadd)
+(define (SWP->int swp)
+  (match-define (SWP size a r rs rn rt) swp)
   (bitwise-ior
     (arithmetic-shift size 30)
     (arithmetic-shift #x7 27)
@@ -66,8 +66,8 @@
   [else (apply SWPB (int->SWPB/struct i))])
 )
 
-(define (SWPB->int ldadd)
-  (match-define (SWPB size a r rs rn rt) ldadd)
+(define (SWPB->int swp)
+  (match-define (SWPB size a r rs rn rt) swp)
   (bitwise-ior
     (arithmetic-shift size 30)
     (arithmetic-shift #x7 27)
@@ -89,3 +89,42 @@
 )
 
 (provide (struct-out SWPB))
+
+(define int->SWPH/struct int->SWP/struct)
+
+(define (int->SWPH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply SWPH (int->SWPH/struct i))])
+)
+
+(define (SWPH->int swp)
+  (match-define (SWPH size a r rs rn rt) swp)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift 1 15)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct SWPH (size a r rs rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int SWPH->int
+  #:property prop:try-from-int int->SWPH
+)
+
+(provide (struct-out SWPH))
