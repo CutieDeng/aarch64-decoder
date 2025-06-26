897c8678ef32e445c3101d34426eb7d9665c8b4e
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 15:56:10 +0800

    (feat "LDCLR" "LDCLRP")

--- lib/mem/atomic.rkt
+++ lib/mem/atomic.rkt
@@ -245,3 +245,52 @@
 )
 
 (provide (struct-out LDCLRH))
+
+(define (int->LDCLRP/struct i)
+  (list (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 23 24)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 15 16)
+    (bitwise-bit-field i 12 15)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDCLRP i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) 0)
+    (equal? (bitwise-bit-field i 30 31) 0)
+    (equal? (bitwise-bit-field i 24 30) #x19)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) 1)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDCLRP (int->LDCLRP/struct i))])
+)
+
+(define (LDCLRP->int ldclr)
+  (match-define (LDCLRP s a r rt2 o3 opc rn rt) ldclr)
+  (bitwise-ior
+    (arithmetic-shift s 30)
+    (arithmetic-shift #x19 24)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rt2 16)
+    (arithmetic-shift o3 15)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDCLRP (s a r rt2 o3 opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int LDCLRP->int
+  #:property prop:try-from-int int->LDCLRP
+)
+
+(provide (struct-out LDCLRP))
