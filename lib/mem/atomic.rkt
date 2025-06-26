a8b6ed2cd475d731f17cc394c9dc580dc7b30d75
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:07:00 +0800

    (feat "LDUMAX" "LDUMAX,LDUMAXB,LDUMAXH")

--- lib/mem/atomic.rkt
+++ lib/mem/atomic.rkt
@@ -811,3 +811,120 @@
 )
 
 (provide (struct-out LDSMINH))
+
+(define int->LDUMAX/struct int->LDADD/struct)
+
+(define (int->LDUMAX i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) #x6)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDUMAX (int->LDUMAX/struct i))])
+)
+
+(define (LDUMAX->int ldadd)
+  (match-define (LDUMAX size a r rs opc rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDUMAX (size a r rs opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int LDUMAX->int
+  #:property prop:try-from-int int->LDUMAX
+)
+
+(provide (struct-out LDUMAX))
+
+(define int->LDUMAXB/struct int->LDUMAX/struct)
+
+(define (int->LDUMAXB i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 0)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) #x6)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDUMAXB (int->LDUMAXB/struct i))])
+)
+
+(define (LDUMAXB->int ldadd)
+  (match-define (LDUMAXB size a r rs opc rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDUMAXB (size a r rs opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int LDUMAXB->int
+  #:property prop:try-from-int int->LDUMAXB
+)
+
+(provide (struct-out LDUMAXB))
+
+(define int->LDUMAXH/struct int->LDUMAX/struct)
+
+(define (int->LDUMAXH i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) #x6)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDUMAXH (int->LDUMAXH/struct i))])
+)
+
+(define (LDUMAXH->int ldadd)
+  (match-define (LDUMAXH size a r rs opc rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDUMAXH (size a r rs opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int LDUMAXH->int
+  #:property prop:try-from-int int->LDUMAXH
+)
+
+(provide (struct-out LDUMAXH))
