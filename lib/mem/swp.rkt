3ad56f2838ecc9ce5460450cb79abb61943048b3
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:24:11 +0800

    (feat "SWP" "SWPB")

--- lib/mem/swp.rkt
+++ lib/mem/swp.rkt
@@ -50,3 +50,42 @@
 )
 
 (provide (struct-out SWP))
+
+(define int->SWPB/struct int->SWP/struct)
+
+(define (int->SWPB i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) 0)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply SWPB (int->SWPB/struct i))])
+)
+
+(define (SWPB->int ldadd)
+  (match-define (SWPB size a r rs rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift 1 15)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct SWPB (size a r rs rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int SWPB->int
+  #:property prop:try-from-int int->SWPB
+)
+
+(provide (struct-out SWPB))
