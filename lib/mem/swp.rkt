3c10c18eac7e311cb18e51112dd3f2404e1a83a4
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 22:23:06 +0800

    (FEAT "SWP")

---
+++ lib/mem/swp.rkt
@@ -0,0 +1,52 @@
+#lang racket
+
+(require "../util/in-feature.rkt")
+(require "../util/into-int.rkt")
+(require "../util/try-from-int.rkt")
+
+(define (int->SWP/struct i)
+  (list
+    (bitwise-bit-field i 30 32)
+    (bitwise-bit-field i 23 24)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->SWP i)
+  (cond [(nand (equal? (bitwise-bit-field i 31 32) #x1)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply SWP (int->SWP/struct i))])
+)
+
+(define (SWP->int ldadd)
+  (match-define (SWP size a r rs rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift size 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift 1 15)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct SWP (size a r rs rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE . #t))
+  #:property prop:into-int SWP->int
+  #:property prop:try-from-int int->SWP
+)
+
+(provide (struct-out SWP))
