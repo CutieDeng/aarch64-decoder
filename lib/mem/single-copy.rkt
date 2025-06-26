40036f5dfb2329c5ae4a474d4f1ab7020fcecc50
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:17:44 +0800

    (feat "LD64B" "LD64BV")

--- lib/mem/single-copy.rkt
+++ lib/mem/single-copy.rkt
@@ -89,3 +89,48 @@
 )
 
 (provide (struct-out ST64B))
+
+(define (int->LD64BV/struct i)
+  (list
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LD64BV i)
+  (cond [(nand (equal? (bitwise-bit-field i 30 32) #x3)
+    (equal? (bitwise-bit-field i 27 30) #x7)
+    (equal? (bitwise-bit-field i 26 27) 0)
+    (equal? (bitwise-bit-field i 24 26) 0)
+    (equal? (bitwise-bit-field i 23 22) 0)
+    (equal? (bitwise-bit-field i 22 23) 0)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) #x3)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LD64BV (int->LD64BV/struct i))])
+)
+
+(define (LD64BV->int ldadd)
+  (match-define (LD64BV rs rn rt) ldadd)
+  (bitwise-ior
+    (arithmetic-shift #x3 30)
+    (arithmetic-shift #x7 27)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift 1 15)
+    (arithmetic-shift #x3 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LD64BV (rs rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LS64_V . #t))
+  #:property prop:into-int LD64BV->int
+  #:property prop:try-from-int int->LD64BV
+)
+
+(provide (struct-out LD64BV))
