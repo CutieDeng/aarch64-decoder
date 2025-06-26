08ee1e8496de2dc08b1fca96161d7493c70fc1af
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:28:12 +0800

    (feat "SWP" "SWPP")

--- lib/mem/swp.rkt
+++ lib/mem/swp.rkt
@@ -128,3 +128,54 @@
 )
 
 (provide (struct-out SWPH))
+
+(define (int->SWPP/struct i)
+  (list
+    (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 23 24)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 15 16)
+    (bitwise-bit-field i 12 15)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+
+(define (int->SWPP i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) 0)
+    (equal? (bitwise-bit-field i 30 31) 0)
+    (equal? (bitwise-bit-field i 24 30) #x19)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 1)
+    (equal? (bitwise-bit-field i 12 15) 0)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply SWPP (int->SWPP/struct i))])
+)
+
+(define (SWPP->int swp)
+  (match-define (SWPP s a r rt2 o3 opc rn rt) swp)
+  (bitwise-ior
+    (arithmetic-shift s 30)
+    (arithmetic-shift #x19 24)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rt2 16)
+    (arithmetic-shift o3 15)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct SWPP (s a r rt2 o3 opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE128 . #t))
+  #:property prop:into-int SWPP->int
+  #:property prop:try-from-int int->SWPP
+)
+
+(provide (struct-out SWPP))
