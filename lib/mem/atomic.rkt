99f70ead78c387bfa48433346cf56b7acdd71850
Cutie Deng <CutieDeng@iCloud.com>
2025-06-26 16:01:14 +0800

    (feat "LDSET" "LDSETP")

--- lib/mem/atomic.rkt
+++ lib/mem/atomic.rkt
@@ -528,3 +528,52 @@
 )
 
 (provide (struct-out LDSETH))
+
+(define (int->LDSETP/struct i)
+  (list (bitwise-bit-field i 30 31)
+    (bitwise-bit-field i 23 24)
+    (bitwise-bit-field i 22 23)
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 15 16)
+    (bitwise-bit-field i 12 15)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LDSETP i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) 0)
+    (equal? (bitwise-bit-field i 30 31) 0)
+    (equal? (bitwise-bit-field i 24 30) #x19)
+    (equal? (bitwise-bit-field i 21 22) 1)
+    (equal? (bitwise-bit-field i 15 16) 0)
+    (equal? (bitwise-bit-field i 12 15) #x3)
+    (equal? (bitwise-bit-field i 10 12) 0)
+  ) #f]
+  [else (apply LDSETP (int->LDSETP/struct i))])
+)
+
+(define (LDSETP->int ldclr)
+  (match-define (LDSETP s a r rt2 o3 opc rn rt) ldclr)
+  (bitwise-ior
+    (arithmetic-shift s 30)
+    (arithmetic-shift #x19 24)
+    (arithmetic-shift a 23)
+    (arithmetic-shift r 22)
+    (arithmetic-shift 1 21)
+    (arithmetic-shift rt2 16)
+    (arithmetic-shift o3 15)
+    (arithmetic-shift opc 12)
+    (arithmetic-shift rn 5)
+    rt
+  )
+)
+
+(struct LDSETP (s a r rt2 o3 opc rn rt)
+  #:transparent
+  #:property prop:in-feature #hash((FEAT_LSE128 . #t))
+  #:property prop:into-int LDSETP->int
+  #:property prop:try-from-int int->LDSETP
+)
+
+(provide (struct-out LDSETP))
