f0bb967fc4b5e0823948cd693e32bbe1af83ef1d
Cutie Deng <CutieDeng@iCloud.com>
2025-07-03 11:45:27 +0800

    (feat "MTE" "GMI")

--- lib/mem/tag.rkt
+++ lib/mem/tag.rkt
@@ -46,3 +46,48 @@
 )
 
 (provide (struct-out ADDG))
+
+(define (int->GMI/struct i)
+  (list
+    (bitwise-bit-field i 16 21)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->GMI i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) #x1)
+    (equal? (bitwise-bit-field i 30 31) #x0)
+    (equal? (bitwise-bit-field i 29 30) #x0)
+    (equal? (bitwise-bit-field i 21 29) #xd6)
+    (equal? (bitwise-bit-field i 15 16) #x0)
+    (equal? (bitwise-bit-field i 14 15) #x0)
+    (equal? (bitwise-bit-field i 13 14) #x0)
+    (equal? (bitwise-bit-field i 12 13) #x1)
+    (equal? (bitwise-bit-field i 11 12) #x0)
+    (equal? (bitwise-bit-field i 10 11) #x1)
+  ) #f]
+  [else (apply GMI (int->GMI/struct i))])
+)
+
+(define (GMI->int rcw)
+  (match-define (GMI xm xn xd) rcw)
+  (bitwise-ior
+    (arithmetic-shift 1 31)
+    (arithmetic-shift #xd6 21)
+    (arithmetic-shift xm 16)
+    (arithmetic-shift #x1 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift xn 5)
+    xd
+  )
+)
+
+(struct GMI (xm xn xd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MTE
+  #:property prop:into-int GMI->int
+  #:property prop:try-from-int int->GMI
+)
+
+(provide (struct-out GMI))
