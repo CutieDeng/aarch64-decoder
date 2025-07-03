1b3112be703aaff5a15a5a7f6833ee13c303405c
Cutie Deng <CutieDeng@iCloud.com>
2025-07-03 11:47:00 +0800

    (feat "MTE" "IRG")

--- lib/mem/tag.rkt
+++ lib/mem/tag.rkt
@@ -91,3 +91,43 @@
 )
 
 (provide (struct-out GMI))
+
+(define int->IRG/struct int->GMI/struct)
+
+(define (int->IRG i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) #x1)
+    (equal? (bitwise-bit-field i 30 31) #x0)
+    (equal? (bitwise-bit-field i 29 30) #x0)
+    (equal? (bitwise-bit-field i 21 29) #xd6)
+    (equal? (bitwise-bit-field i 15 16) #x0)
+    (equal? (bitwise-bit-field i 14 15) #x0)
+    (equal? (bitwise-bit-field i 13 14) #x0)
+    (equal? (bitwise-bit-field i 12 13) #x1)
+    (equal? (bitwise-bit-field i 11 12) #x0)
+    (equal? (bitwise-bit-field i 10 11) #x0)
+  ) #f]
+  [else (apply IRG (int->IRG/struct i))])
+)
+
+(define (IRG->int rcw)
+  (match-define (IRG xm xn xd) rcw)
+  (bitwise-ior
+    (arithmetic-shift 1 31)
+    (arithmetic-shift #xd6 21)
+    (arithmetic-shift xm 16)
+    (arithmetic-shift #x1 12)
+    (arithmetic-shift xn 5)
+    xd
+  )
+)
+
+(struct IRG (xm xn xd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MTE
+  #:property prop:into-int IRG->int
+  #:property prop:try-from-int int->IRG
+)
+
+(provide (struct-out IRG))
+
