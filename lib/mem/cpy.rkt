7a98ede94108b2276beae208db21a23137b9b8e3
Cutie Deng <CutieDeng@iCloud.com>
2025-07-03 19:22:26 +0800

    (feat "CPYFP" "CPYFPTN")

--- lib/mem/cpy.rkt
+++ lib/mem/cpy.rkt
@@ -315,3 +315,41 @@
 )
 
 (provide (struct-out CPYFPT))
+
+(define int->CPYFPTN/struct int->CPYFP/struct)
+
+(define (int->CPYFPTN i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x0)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 16) #xf)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply CPYFPTN (int->CPYFPTN/struct i))])
+)
+
+(define (CPYFPTN->int rcw)
+  (match-define (CPYFPTN sz op1 rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift op1 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct CPYFPTN (sz op1 rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int CPYFPTN->int
+  #:property prop:try-from-int int->CPYFPTN
+)
+
+(provide (struct-out CPYFPTN))
