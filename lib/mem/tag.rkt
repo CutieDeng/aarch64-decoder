2fef8001ffefaad1a5a5a3b1ecba01a53df3b18b
Cutie Deng <CutieDeng@iCloud.com>
2025-07-03 11:42:03 +0800

    (feat "MTE" "ADDG")

---
+++ lib/mem/tag.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require "../util/in-feature.rkt")
+(require "../util/into-int.rkt")
+(require "../util/try-from-int.rkt")
+
+(define (int->ADDG/struct i)
+  (list
+    (bitwise-bit-field i 16 22)
+    (bitwise-bit-field i 14 16)
+    (bitwise-bit-field i 10 14)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->ADDG i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 31 32) #x1)
+    (equal? (bitwise-bit-field i 30 31) #x0)
+    (equal? (bitwise-bit-field i 29 30) #x0)
+    (equal? (bitwise-bit-field i 23 29) #x23)
+    (equal? (bitwise-bit-field i 22 23) #x0)
+    (equal? (bitwise-bit-field i 14 16) #x0)
+  ) #f]
+  [else (apply ADDG (int->ADDG/struct i))])
+)
+
+(define (ADDG->int rcw)
+  (match-define (ADDG uimm6 op3 uimm4 xn xd) rcw)
+  (bitwise-ior
+    (arithmetic-shift 1 31)
+    (arithmetic-shift #x23 23)
+    (arithmetic-shift uimm6 16)
+    (arithmetic-shift op3 14)
+    (arithmetic-shift uimm4 10)
+    (arithmetic-shift xn 5)
+    xd
+  )
+)
+
+(struct ADDG (uimm6 op3 uimm4 xn xd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MTE
+  #:property prop:into-int ADDG->int
+  #:property prop:try-from-int int->ADDG
+)
+
+(provide (struct-out ADDG))
