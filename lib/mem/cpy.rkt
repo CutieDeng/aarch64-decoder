3b5d5c3d17bfde43e99124a1979b2ee0f6456d31
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 00:17:18 +0800

    (FEAT "CPYFPRT")

--- lib/mem/cpy.rkt
+++ lib/mem/cpy.rkt
@@ -125,3 +125,41 @@
 )
 
 (provide (struct-out CPYFPRN))
+
+(define int->CPYFPRT/struct int->CPYFP/struct)
+
+(define (int->CPYFPRT i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x0)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 16) #x2)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply CPYFPRT (int->CPYFPRT/struct i))])
+)
+
+(define (CPYFPRT->int rcw)
+  (match-define (CPYFPRT sz op1 rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift op1 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct CPYFPRT (sz op1 rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int CPYFPRT->int
+  #:property prop:try-from-int int->CPYFPRT
+)
+
+(provide (struct-out CPYFPRT))
