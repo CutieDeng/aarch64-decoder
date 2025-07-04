09f01fb5fa808ee42dac6fcad96035978460bf55
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 21:35:51 +0800

    (FEAT "CPYPWTWN")

--- lib/mem/cpy.rkt
+++ lib/mem/cpy.rkt
@@ -1204,3 +1204,42 @@
 )
 
 (provide (struct-out CPYPWTRN))
+
+(define int->CPYPWTWN/struct int->CPYFP/struct)
+
+(define (int->CPYPWTWN i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x1)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 16) #x5)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply CPYPWTWN (int->CPYPWTWN/struct i))])
+)
+
+(define (CPYPWTWN->int rcw)
+  (match-define (CPYPWTWN sz op1 rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 26)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift op1 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct CPYPWTWN (sz op1 rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int CPYPWTWN->int
+  #:property prop:try-from-int int->CPYPWTWN
+)
+
+(provide (struct-out CPYPWTWN))
