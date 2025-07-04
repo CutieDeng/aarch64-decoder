76b0f0b3b65638087ab0a951b24297c261c9f36c
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 23:42:57 +0800

    (FEAT "LD1D")

--- lib/mem/sve/single-continuous.rkt
+++ lib/mem/sve/single-continuous.rkt
@@ -44,3 +44,44 @@
 )
 
 (provide (struct-out LD1B))
+
+(define (int->LD1D/struct i)
+  (list
+    (bitwise-bit-field i 21 24)
+    (bitwise-bit-field i 16 20)
+    (bitwise-bit-field i 10 13)
+    (bitwise-bit-field i 5 10)
+    (bitwise-bit-field i 0 5))
+)
+
+(define (int->LD1D i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 25 32) #x52)
+    (equal? (bitwise-bit-field i 21 25) #xf)
+    (equal? (bitwise-bit-field i 20 21) #x0)
+    (equal? (bitwise-bit-field i 13 16) #x5)
+  ) #f]
+  [else (apply LD1D (int->LD1D/struct i))])
+)
+
+(define (LD1D->int ld1)
+  (match-define (LD1D dtype imm4 pg rn zt) ld1)
+  (bitwise-ior
+    (arithmetic-shift #x52 25)
+    (arithmetic-shift dtype 21)
+    (arithmetic-shift imm4 16)
+    (arithmetic-shift #x5 13)
+    (arithmetic-shift pg 10)
+    (arithmetic-shift rn 5)
+    zt
+  )
+)
+
+(struct LD1D (dtype imm4 pg rn zt)
+  #:transparent
+  #:property prop:in-feature #f
+  #:property prop:into-int LD1D->int
+  #:property prop:try-from-int int->LD1D
+)
+
+(provide (struct-out LD1D))
