69c36634e6265d45bbb2aac611ce5a014d69858d
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 21:19:04 +0800

    (FEAT "CPYP")

--- lib/mem/cpy.rkt
+++ lib/mem/cpy.rkt
@@ -619,3 +619,42 @@
 )
 
 (provide (struct-out CPYFPWTWN))
+
+(define int->CPYP/struct int->CPYFP/struct)
+
+(define (int->CPYP i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x1)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 16) #x0)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply CPYP (int->CPYP/struct i))])
+)
+
+(define (CPYP->int rcw)
+  (match-define (CPYP sz op1 rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 26)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift op1 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct CPYP (sz op1 rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int CPYP->int
+  #:property prop:try-from-int int->CPYP
+)
+
+(provide (struct-out CPYP))
