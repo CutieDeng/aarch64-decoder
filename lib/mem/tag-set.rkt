b7eb93059cfd0a9e9621004b5557a8527bb4f031
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 21:42:47 +0800

    (FEAT "SETGPN")

--- lib/mem/tag-set.rkt
+++ lib/mem/tag-set.rkt
@@ -50,3 +50,43 @@
 )
 
 (provide (struct-out SETGP))
+
+(define int->SETGPN/struct int->SETGP/struct)
+
+(define (int->SETGPN i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x1)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 22 24) #x3)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 14) #x2)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply SETGPN (int->SETGPN/struct i))])
+)
+
+(define (SETGPN->int rcw)
+  (match-define (SETGPN sz rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 26)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift #x3 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct SETGPN (sz rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int SETGPN->int
+  #:property prop:try-from-int int->SETGPN
+)
+
+(provide (struct-out SETGPN))
