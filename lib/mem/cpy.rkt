8c895d56dacab73b33b1b1c7b2eef90423d7c3a6
Cutie Deng <CutieDeng@iCloud.com>
2025-07-04 21:24:10 +0800

    (FEAT "CPYPRTRN")

--- lib/mem/cpy.rkt
+++ lib/mem/cpy.rkt
@@ -814,3 +814,42 @@
 )
 
 (provide (struct-out CPYPRTN))
+
+(define int->CPYPRTRN/struct int->CPYFP/struct)
+
+(define (int->CPYPRTRN i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 27 30) #x3)
+    (equal? (bitwise-bit-field i 26 27) #x1)
+    (equal? (bitwise-bit-field i 24 26) #x1)
+    (equal? (bitwise-bit-field i 21 22) #x0)
+    (equal? (bitwise-bit-field i 12 16) #xa)
+    (equal? (bitwise-bit-field i 10 12) #x1)
+  ) #f]
+  [else (apply CPYPRTRN (int->CPYPRTRN/struct i))])
+)
+
+(define (CPYPRTRN->int rcw)
+  (match-define (CPYPRTRN sz op1 rs op2 rn rd) rcw)
+  (bitwise-ior
+    (arithmetic-shift sz 30)
+    (arithmetic-shift #x3 27)
+    (arithmetic-shift #x1 26)
+    (arithmetic-shift #x1 24)
+    (arithmetic-shift op1 22)
+    (arithmetic-shift rs 16)
+    (arithmetic-shift op2 12)
+    (arithmetic-shift #x1 10)
+    (arithmetic-shift rn 5)
+    rd
+  )
+)
+
+(struct CPYPRTRN (sz op1 rs op2 rn rd)
+  #:transparent
+  #:property prop:in-feature 'FEAT_MOPS
+  #:property prop:into-int CPYPRTRN->int
+  #:property prop:try-from-int int->CPYPRTRN
+)
+
+(provide (struct-out CPYPRTRN))
