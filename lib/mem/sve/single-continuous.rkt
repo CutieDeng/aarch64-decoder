c3b64dcef497b84185c7a9552b451caa3495e459
Cutie Deng <CutieDeng@iCloud.com>
2025-10-26 19:34:01 +0800

    (FEAT "LD1ROD")

--- lib/mem/sve/single-continuous.rkt
+++ lib/mem/sve/single-continuous.rkt
@@ -157,3 +157,39 @@
 )
 
 (provide (struct-out LD1ROB))
+
+(define int->LD1ROD/struct int->LD1ROB/struct)
+
+(define (int->LD1ROD i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 25 32) #x52)
+    (equal? (bitwise-bit-field i 23 25) #x3)
+    (equal? (bitwise-bit-field i 21 23) #x1)
+    (equal? (bitwise-bit-field i 20 21) #x0)
+    (equal? (bitwise-bit-field i 13 16) #x1)
+  ) #f]
+  [else (apply LD1ROD (int->LD1ROD/struct i))])
+)
+
+(define (LD1ROD->int ld1)
+  (match-define (LD1ROD msz ssz imm4 pg rn zt) ld1)
+  (bitwise-ior
+    (arithmetic-shift #x52 25)
+    (arithmetic-shift msz 23)
+    (arithmetic-shift ssz 21)
+    (arithmetic-shift imm4 16)
+    (arithmetic-shift #x1 13)
+    (arithmetic-shift pg 10)
+    (arithmetic-shift rn 5)
+    zt
+  )
+)
+
+(struct LD1ROD (msz ssz imm4 pg rn zt)
+  #:transparent
+  #:property prop:in-feature 'FEAT_F64MM
+  #:property prop:into-int LD1ROD->int
+  #:property prop:try-from-int int->LD1ROD
+)
+
+(provide (struct-out LD1ROD))
