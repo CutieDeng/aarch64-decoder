d2293eb3eabf4130c6a9ec58054ac95d4968694d
Cutie Deng <CutieDeng@iCloud.com>
2025-10-26 21:10:27 +0800

    (FEAT "LDFF1H")

--- lib/mem/sve/single-continuous.rkt
+++ lib/mem/sve/single-continuous.rkt
@@ -849,3 +849,36 @@
 )
 
 (provide (struct-out LDFF1D))
+
+(define int->LDFF1H/struct int->LDFF1B/b/struct)
+
+(define (int->LDFF1H i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 25 32) #x52)
+    (equal? (bitwise-bit-field i 21 25) #x5)
+    (equal? (bitwise-bit-field i 13 16) #x3)
+  ) #f]
+  [else (apply LDFF1H (int->LDFF1H/struct i))])
+)
+
+(define (LDFF1H->int ld1)
+  (match-define (LDFF1H dtype rm pg rn zt) ld1)
+  (bitwise-ior
+    (arithmetic-shift #x52 25)
+    (arithmetic-shift dtype 21)
+    (arithmetic-shift rm 16)
+    (arithmetic-shift #x3 13)
+    (arithmetic-shift pg 10)
+    (arithmetic-shift rn 5)
+    zt
+  )
+)
+
+(struct LDFF1H (dtype rm pg rn zt)
+  #:transparent
+  #:property prop:in-feature 'FEAT_SVE
+  #:property prop:into-int LDFF1H->int
+  #:property prop:try-from-int int->LDFF1H
+)
+
+(provide (struct-out LDFF1H))
