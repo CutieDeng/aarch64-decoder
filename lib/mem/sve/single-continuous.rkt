f824a570147415ed422c8d4fa4801ae20648918f
Cutie Deng <CutieDeng@iCloud.com>
2025-10-26 19:38:13 +0800

    (FEAT "LD1ROH")

--- lib/mem/sve/single-continuous.rkt
+++ lib/mem/sve/single-continuous.rkt
@@ -193,3 +193,39 @@
 )
 
 (provide (struct-out LD1ROD))
+
+(define int->LD1ROH/struct int->LD1ROB/struct)
+
+(define (int->LD1ROH i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 25 32) #x52)
+    (equal? (bitwise-bit-field i 23 25) #x1)
+    (equal? (bitwise-bit-field i 21 23) #x1)
+    (equal? (bitwise-bit-field i 20 21) #x0)
+    (equal? (bitwise-bit-field i 13 16) #x1)
+  ) #f]
+  [else (apply LD1ROH (int->LD1ROH/struct i))])
+)
+
+(define (LD1ROH->int ld1)
+  (match-define (LD1ROH msz ssz imm4 pg rn zt) ld1)
+  (bitwise-ior
+    (arithmetic-shift #x52 25)
+    (arithmetic-shift msz 23)
+    (arithmetic-shift ssz 21)
+    (arithmetic-shift imm4 16)
+    (arithmetic-shift #x1 13)
+    (arithmetic-shift pg 10)
+    (arithmetic-shift rn 5)
+    zt
+  )
+)
+
+(struct LD1ROH (msz ssz imm4 pg rn zt)
+  #:transparent
+  #:property prop:in-feature 'FEAT_F64MM
+  #:property prop:into-int LD1ROH->int
+  #:property prop:try-from-int int->LD1ROH
+)
+
+(provide (struct-out LD1ROH))
