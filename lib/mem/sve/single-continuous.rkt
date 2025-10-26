f80f3250712a4756cd2b0b8e9793c8ee5bb66a73
Cutie Deng <CutieDeng@iCloud.com>
2025-10-26 21:19:00 +0800

    (FEAT "LDNF1B")

--- lib/mem/sve/single-continuous.rkt
+++ lib/mem/sve/single-continuous.rkt
@@ -1014,3 +1014,38 @@
 )
 
 (provide (struct-out LDFF1W))
+
+(define int->LDNF1B/struct int->LD1SB/struct)
+
+(define (int->LDNF1B i)
+  (cond [(nand 
+    (equal? (bitwise-bit-field i 25 32) #x52)
+    (equal? (bitwise-bit-field i 21 25) #x0)
+    (equal? (bitwise-bit-field i 20 21) #x1)
+    (equal? (bitwise-bit-field i 13 16) #x5)
+  ) #f]
+  [else (apply LDNF1B (int->LDNF1B/struct i))])
+)
+
+(define (LDNF1B->int ld1)
+  (match-define (LDNF1B dtype imm4 pg rn zt) ld1)
+  (bitwise-ior
+    (arithmetic-shift #x52 25)
+    (arithmetic-shift dtype 21)
+    (arithmetic-shift #x1 20)
+    (arithmetic-shift imm4 16)
+    (arithmetic-shift #x5 13)
+    (arithmetic-shift pg 10)
+    (arithmetic-shift rn 5)
+    zt
+  )
+)
+
+(struct LDNF1B (dtype imm4 pg rn zt)
+  #:transparent
+  #:property prop:in-feature 'FEAT_SVE
+  #:property prop:into-int LDNF1B->int
+  #:property prop:try-from-int int->LDNF1B
+)
+
+(provide (struct-out LDNF1B))
